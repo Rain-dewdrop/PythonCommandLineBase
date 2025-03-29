e6ef6782917840cbccf634ed18c0385d75964cd1
Rain_dewdrop <3350941618@qq.com>
2025-03-29 19:08:23 +0800

    Update log.py

--- libs/log/log.py
+++ libs/log/log.py
@@ -2,5 +2,5 @@ import datetime
 from libs.conifg import ProjectConfigPath,RootName
 def log(text,mode='INFO'):
     if str.upper(mode) == 'INFO' or str.upper(mode) == 'WARN':
-        prefix = f"{RootName} {datetime.datetime.now()}] [{str.upper(mode)}]"
+        prefix = f"{RootName} {datetime.datetime.now()} [{str.upper(mode)}]"
         print(f'{prefix} {text}')
