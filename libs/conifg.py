eb83e61bfab623f595f77b742f2beb85e5c25501
Rain_dewdrop <3350941618@qq.com>
2025-03-29 19:03:51 +0800

    Add files via upload

--- libs/conifg.py
+++ libs/conifg.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+from libs.readjson import readjson
 import os
 RootPath = Path(__file__).resolve().parent.parent
 ProjectConfigPath = os.path.join(RootPath, 'ProjectConfig.json')
+RootName = readjson(ProjectConfigPath,'RootName')
+Version = readjson(ProjectConfigPath,'Version')
+VersionType = readjson(ProjectConfigPath,'VersionType')
+Author = readjson(ProjectConfigPath,'Author')
+GitHubUrl = readjson(ProjectConfigPath,'GitHubUrl')
+BiliBiliUrl = readjson(ProjectConfigPath,'BiliBiliUrl')
--- libs/log/log.py
+++ libs/log/log.py
@@ -1,7 +1,6 @@
 import datetime
-from libs.readjson import readjson
-from libs.conifg import ProjectConfigPath
+from libs.conifg import ProjectConfigPath,RootName
 def log(text,mode='INFO'):
     if str.upper(mode) == 'INFO' or str.upper(mode) == 'WARN':
-        prefix = f"{readjson(ProjectConfigPath,'RootName')} {datetime.datetime.now()}] [{mode}]"
+        prefix = f"{RootName} {datetime.datetime.now()}] [{str.upper(mode)}]"
         print(f'{prefix} {text}')
